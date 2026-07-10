fb2ed954c55fb9dd0abe83f2786a1158c53b4df5
Hp Estudio <gagodario1@gmail.com>
2026-07-10 15:16:43 -0400

    Ejemplo de .env

--- read_docx.py
+++
@@ -1,9 +0,0 @@
-import docx
-import sys
-
-# Set UTF-8 encoding for output
-sys.stdout.reconfigure(encoding='utf-8')
-
-doc = docx.Document('Clínica del PC.docx')
-for paragraph in doc.paragraphs:
-    print(paragraph.text)
